f146923be33de5fd0f10ab1a69f8bd33420b3d0e
PrashantGaur84 <prashantgaur20786@gmail.com>
2026-01-13 20:38:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import addressRouter from './Routes/address.js'
 import paymentRouter from './Routes/payment.js'
 import cors from 'cors';
 
-mongoose.connect("mongodb+srv://prashantgaur0019:lfWmonso9y43Dsom@cluster0.au2qexn.mongodb.net/" , {
+mongoose.connect("mongodb+srv://prashantgaur20786_db_user:lfWmonso9y43Dsom@cluster0.x8k6puj.mongodb.net/" , {
     dbName:"E_Commerce"
 }).then(()=>console.log("MongoDB Connected Successfully")).catch((err)=>console.log(err));
 
